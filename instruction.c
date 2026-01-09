3977b2ce2d70cac7e7603ed692b2991d1d6476bf
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-01-09 11:56:40 +0530

    Add files via upload

---
+++ instruction.c
@@ -0,0 +1,60 @@
+ int age;
+ int i = 10,j = 25;// multiple value  can be written 
+ float temperature = 98.6;// for decimal used float
+ float a = 1.5, b = 1.99+ 2.4* 144;// operations can be written
+ float p = 5, q = p * 6;// first declare p then asiign it further
+ char name[50];
+ int x, y, z = 5;
+
+
+ //++++++OPERATIONS++++++++++
+
+ int ad;
+ float kot, delta , alpha, beta, gamma;
+ ad = 3200;
+ kot = 0.0056;
+ delta = alpha * beta / gamma + 3.2 * 2/5;
+
+
+ //++++++TYPE CONVERSION++++++++
+ int a,b = 2;
+ float x = 9.5;
+ double y = 10.5;
+ long int z = 50;
+ double d;
+ a = z/b+b*x-y;
+ printf("integer :  %d\n",a);
+ return 0 ;
+
+ //++++++HIERARCHY OF OPERATIONS+++++++
+  
+ i = 2*3/4+4+4/4+8-2+5/8
+//| Priority | Operators  
+// 1st  */%  multiplication,division,modular division,addition,subtraction //left to right
+// 2 nd + -  addition,subtraction // left to right
+//3rd = assigment
+ 
+
+// Operator symbol
+//+= Addtion assigment x+= 4;  x = x +4;
+//-= Subtraction assigment x-=4; x = x - 4;
+//Multiplication assigment x*=4; x = x *4;
+//Division assigment x/=4; x = x/4;
+//Remainder assigment x%=4; x = x%4;
+
+
+
+
+
+
+// Converts a positive value to a negative
+// Increment the value by 1 and then use it in our statement
+// Decrement the value by 1 and then use it in our statement
+// Use current value in the statement and then increment it by 1
+// Use current value in the statement and then decrement it by 1
+// Example
+// X = -У
+// × = ++y
+// x = --У
+// x = y++
+// x= У--
