cae1c0f662c4579f2a0f839dd075238bbe6290da
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-03-23 16:16:56 +0000

    changes

--- 01_c_language/02_print.c
+++ 01_c_language/02_print.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-  printf("Integer: %d\n",10);//Use \n within the string to move to a new line.
+  printf("Integer: %d\n",10);//Use \n within the string to move to a new line. escape sequence
   printf("Character: %c\n",'A');
   printf("Floating-point: %f\n",3.14);
 
