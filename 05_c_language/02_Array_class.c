276dde2b55d692650eb2c124d7b62b4a10cf6899
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-03-27 16:48:36 +0000

    Bubble sort

--- 05_c_language/02_Array_class.c
+++ 05_c_language/02_Array_class.c
@@ -225,6 +225,29 @@ pass = (n-1)   where n=size of array
 */
 
 
+#include<stdio.h>
+main()
+{
+    int a[10],i,n,data,index;
+    printf("how many elements");
+    scanf("%d",&n);
+    printf("enter %d elements",n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        printf("enter new data u want to insert");
+        scanf("%d",&data);
+        printf("enter index at which u want to insert");
+        scanf("%d",&index);
+        for(i=n-1;i>=index;i--)
+            a[i+1] =a[i];
+            a[index]=data;
+        n++;
+        printf("after insertion ");
+        for(i=0;i<n;i++)
+            printf("%d ",a[i]);
+        
+}
+
 
 
 
