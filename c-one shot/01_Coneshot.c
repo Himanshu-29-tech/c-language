a898292565cc1d7418452a3a4acc0bd2b8b9ac3c
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-04-20 15:17:05 +0000

    one shot c

---
+++ c-one shot/01_Coneshot.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("Helo Himanshu\n");
+    printf("Hello Rahul"); // for next line using \n. -->> but itself never print
+
+
+    printf("nn\n\nnn\n");
+    printf("nn/n/nnn/n");
+
+    int x; // variable declaration
+    x = 5; // putting the variable value
+    printf("%d\n",x); // printing the variable
+
+    x = 9;
+    printf("%d",x); // now value of new variable will be print that is 9.
+
+    x = x + 6;
+    printf("%d",x);
+
+
+    x = x - 20;
+    printf("%d",x);  
+
+
+    // Airthmatic operations on in data type
+    int x = 5;
+    int y = 2;
+    printf("%d\n",x + y);
+    printf("%d\n", x - y);
+
+    printf("%d\n", x * y);
+
+    printf("%d\n",x/y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
