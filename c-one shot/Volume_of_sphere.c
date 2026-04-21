12454f9380faa5fc1a1c60a175aab546c387e1c3
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-04-21 13:17:44 +0000

    Problems

--- c-one shot/Volume_of_sphere.c
+++ c-one shot/Volume_of_sphere.c
@@ -1,3 +1,45 @@
 #include<stdio.h>
 int main() {
   
+int r = 5;
+int v = 4*3.14*r*r*r / 3;
+printf("The volume is : %f", v);
+
+/* H.W ____________________________
+WAP to display area of circle with given radius */
+#include <stdio.h>
+int main(){
+    int Radius,Area;
+printf("Radius:");
+scanf("%d",&Radius);
+Area = 3.14 * Radius * Radius;
+printf("Area:%d",Area);
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+
+}
